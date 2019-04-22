6353e54befe70a7af2e881ba57b62fcfc4b6da80
CLARE PINDER <CLARE.PINDER@LIVE.CO.UK>
2019-04-22 15:18:56 +0100

    builing learning model

--- gym-connect4/connect4_understanding.py
+++ gym-connect4/connect4_understanding.py
@@ -14,15 +14,15 @@ env = gym.make('Connect4-v0')
 env.reset()
 MAX_STEPS = 21 # half of 7 x 6 board
 SCORE_REQUIREMENT = 1 # This is the score that means Ulima has done well. Could do it as a proportion of wins..?
-INITIAL_GAMES = 100
+INITIAL_GAMES = 3
 rand = random.Random()
 
-class Ulima():
+class TrainUlima():
 
     def model_data_preparation(self):
         # for each play we want to store the state of the board and the move
         self.training_data = []
-        self.accepted_scores = []
+        self.scores = []
         for game_index in range(INITIAL_GAMES): # start by playing 10,000 games
             score = 0
             self.previous_observation = []
@@ -40,31 +40,32 @@ class Ulima():
                 self.training_data += [copy_observation, hot_action]
 
                 score += reward
-                self.accepted_scores.append(score) # maybe can get rid of score and just add reward.
+                self.scores.append(score) # maybe can get rid of score and just add reward.
                 if done:
                     break
 
             env.reset()
 
-        print(self.accepted_scores)
-        # return self.training_data
+        print(self.scores)
+        return self.training_data
 
 
-    # def build_model(input_size, output_size):
-    #     model = Sequential()
-    #     model.add(Dense(128, input_dim=input_size, activation='relu'))
-    #     model.add(Dense(52, activation='relu'))
-    #     model.add(Dense(output_size, activation='linear'))
-    #     model.compile(loss='mse', optimizer=Adam())
-    #     return model
-    #
-    #
-    # def train_model(self.training_data):
-    #     X = np.array([i[0] for i in self.training_data]).reshape(-1, len(self.training_data[0][0]))
-    #     y = np.array([i[1] for i in self.training_data]).reshape(-1, len(self.training_data[0][1]))
-    #     model = build_model(input_size=len(X[0]), output_size=len(y[0]))
-    #
-    #     model.fit(X, y, epochs=10)
-    #     return model
-    #
-    # trained_model = train_model(self.training_data)
+    def build_model(self, input_size, output_size):
+        model = Sequential()
+        model.add(Dense(128, input_dim=input_size, activation='relu'))
+        model.add(Dense(52, activation='relu'))
+        model.add(Dense(output_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam())
+        return model
+
+
+    def train_model(self, training_data):
+        X = np.array([i[0] for i in training_data]).reshape(training_data[i][0], [-1])
+        y = np.array([i[1] for i in training_data]).reshape(training_data[i][1], [-1])
+        model = build_model(input_size=len(X[0]), output_size=len(y[0]))
+
+        model.fit(X, y, epochs=10)
+        return model
+
+u = TrainUlima()
+trained_model = u.train_model(u.model_data_preparation())
