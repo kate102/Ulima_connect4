6c52f10e58a47d386e983d4f50381b1b8190eec7
CLARE PINDER <CLARE.PINDER@LIVE.CO.UK>
2019-04-22 11:12:57 +0100

    tidied directory

--- gym-connect4/connect4_understanding.py
+++ gym-connect4/connect4_understanding.py
@@ -12,59 +12,62 @@ from keras.optimizers import Adam
 
 env = gym.make('Connect4-v0')
 env.reset()
-max_steps = 21 # half of 7 x 6 board
-score_requirement = 1 # This is the score that means Ulima has done well
-intial_games = 10000
+MAX_STEPS = 21 # half of 7 x 6 board
+SCORE_REQUIREMENT = 1 # This is the score that means Ulima has done well. Could do it as a proportion of wins..?
+INITIAL_GAMES = 5
 
-def model_data_preparation():
-    training_data = []
-    accepted_scores = []
-    for game_index in range(intial_games): # start by playing 10,000 games
-        score = 0
-        game_memory = []
-        previous_observation = []
-        for step_index in range(max_steps):
-            action = env.action_space.sample() # can we change this to .get_avail_moves
-            observation, reward, done, info = env.step(action)
+class Ulima():
 
-            if len(previous_observation) > 0: # store the prev observation if there is one
-                game_memory.append([previous_observation, action])
+    def model_data_preparation(self):
+        self.training_data = []
+        self.accepted_scores = []
+        for game_index in range(INITIAL_GAMES): # start by playing 10,000 games
+            score = 0
+            self.game_memory = []
+            self.previous_observation = []
 
-            previous_observation = observation
-            score += reward
-            if done:
-                break
+            for step_index in range(MAX_STEPS):
+                action = env.action_space.sample() # can we change this to .get_avail_moves
+                observation, reward, done, info = env.step(action)
 
-        if score >= score_requirement:
-            accepted_scores.append(score) # if Ulima won the game then one hot encode the last action
-            for data in game_memory:
-                action = data[1]
-                output = [0, 0, 0, 0, 0, 0]
-                output.insert(action, 1)
-                training_data.append([data[0], output])
+                if len(self.previous_observation) > 0: # store the prev observation if there is one
+                    self.game_memory.append([self.previous_observation, action])
 
-        env.reset()
+                self.previous_observation = observation
+                score += reward
+                if done:
+                    break
 
-    print(accepted_scores)
+            if score >= SCORE_REQUIREMENT: # only saves won games but should probs also save lost games
+                self.accepted_scores.append(score) # if Ulima won the game then one hot encode the last action
+                for data in self.game_memory:
+                    action = data[1]
+                    hot_move = [0, 0, 0, 0, 0, 0]
+                    hot_move.insert(action, 1)
+                    self.training_data.append([data[0], hot_move]) # should this be self.previous_observation in stead of data[0]
 
-    return training_data
+            env.reset()
+
+        print(self.accepted_scores)
+
+        return self.training_data
 
 
-# def build_model(input_size, output_size):
-#     model = Sequential()
-#     model.add(Dense(128, input_dim=input_size, activation='relu'))
-#     model.add(Dense(52, activation='relu'))
-#     model.add(Dense(output_size, activation='linear'))
-#     model.compile(loss='mse', optimizer=Adam())
-#     return model
-#
-#
-# def train_model(training_data):
-#     X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
-#     y = np.array([i[1] for i in training_data]).reshape(-1, len(training_data[0][1]))
-#     model = build_model(input_size=len(X[0]), output_size=len(y[0]))
-#
-#     model.fit(X, y, epochs=10)
-#     return model
-#
-# trained_model = train_model(training_data)
+    # def build_model(input_size, output_size):
+    #     model = Sequential()
+    #     model.add(Dense(128, input_dim=input_size, activation='relu'))
+    #     model.add(Dense(52, activation='relu'))
+    #     model.add(Dense(output_size, activation='linear'))
+    #     model.compile(loss='mse', optimizer=Adam())
+    #     return model
+    #
+    #
+    # def train_model(self.training_data):
+    #     X = np.array([i[0] for i in self.training_data]).reshape(-1, len(self.training_data[0][0]))
+    #     y = np.array([i[1] for i in self.training_data]).reshape(-1, len(self.training_data[0][1]))
+    #     model = build_model(input_size=len(X[0]), output_size=len(y[0]))
+    #
+    #     model.fit(X, y, epochs=10)
+    #     return model
+    #
+    # trained_model = train_model(self.training_data)
